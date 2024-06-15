6d9d74223a26c2c1da0edc46d1518a3aa21622ea
Brady Bates <brady.bates@my.uwrf.edu>
2024-06-15 03:48:35 -0500

    Added Axios to handle outbound HTTP requests

--- base.js
+++
@@ -1,5 +0,0 @@
-// import * as http from "http";
-var message = 'Hello, World!';
-var heading = document.createElement('h1');
-heading.textContent = message;
-document.body.appendChild(heading);
--- runtime.js
+++ runtime.js
@@ -1,8 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var app = (0, express_1.default)();
-app.get('/', function (req, res) {
-    res.send('Hello World');
+var Express = require('express');
+// const Key_file = require('./riot_key.js');
+var Axios = require('axios');
+var APP = Express();
+var PORT = 3000;
+var riot_key_js_1 = require("./riot_key.js");
+var API_URL = "https://na1.api.riotgames.com";
+var request_param = "/" + "riot_tuxedo";
+var request_url = "/riot/account/v1/accounts/me" +
+    "?api_key=" + riot_key_js_1.API_KEY;
+Axios({
+    method: 'get',
+    url: API_URL + request_url
+})
+    .finally(function (response) {
+    if (typeof response !== undefined) {
+        console.log("Data: " + response.data
+            + "\nStatus: " + response.status);
+    }
+    else {
+        console.log(typeof response + 2);
+    }
 });
-app.listen(3000);
+/*  GET request to root dir  */
+APP.get('/', function (req, res) {
+    res.send();
+});
+// Starts server on $port=3000
+APP.listen(PORT, function () {
+    console.log("Server is running at http://localhost:".concat(PORT));
+});
